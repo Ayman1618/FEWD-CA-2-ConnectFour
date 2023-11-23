b2a0a10e4906ffe3d3494427b716064ad4d27c1a
Ayman1618 <144336449+Ayman1618@users.noreply.github.com>
2023-11-23 17:51:49 +0530

    Add files via upload

---
+++ main page/main.js
@@ -0,0 +1,177 @@
+
+
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+var currColumns = []; //keeps track of which row each column is at.
+
+const openingAudio = new Audio("../sounds/suits-you-69233.mp3")
+
+
+
+window.onload = function() {
+    setGame();
+    openingAudio.pause()
+     openingAudio.currentTime = 0;
+     openingAudio.loop = true;
+    openingAudio.play()
+ }
+
+ // mute functionality
+const audio = document.querySelector(".mute")
+
+let sound = localStorage.getItem("sound")
+console.log(sound)
+
+audio.onclick = function(){
+    if(sound)
+    {
+        audio.src = "../images/mute.png"
+        localStorage.setItem("sound",false)
+        sound = false
+        openingAudio.pause()
+    }
+    else{
+        audio.src = "../images/mute.png"
+        localStorage.setItem("sound",true)
+        sound = true
+        openingAudio.currentTime = 0
+        openingAudio.play()
+    }}
+
+
+function setGame() {
+    board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
+            // HTML
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+    changeDisc()
+    //get coords of that tile clicked
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    // figure out which row the current column should be on
+    r = currColumns[c]; 
+
+    if (r < 0) { // board[r][c] != ' '
+        return;
+    }
+
+    board[r][c] = currPlayer; //update JS board
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerYellow;
+        const drop = new Audio("../sounds/very-lush-and-swag-loop-74140.mp3");
+        drop.play();
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = playerRed;
+    }
+
+    r -= 1; //update the row height for that column
+    currColumns[c] = r; //update the array
+
+    checkWinner();
+}
+
+function checkWinner() {
+     // horizontal
+     for (let r = 0; r < rows; r++) {
+         for (let c = 0; c < columns - 3; c++){
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+         }
+    }
+
+    // vertical
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // anti diagonal
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // diagonal
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    // if (board[r][c] == playerRed) {
+    //     winner.innerText = "Red Wins";             
+    // } else {
+    //     winner.innerText = "Yellow Wins";
+    // }
+    gameOver = true;
+}
+
+const change =  document.querySelector(".disc-indicator")
+
+function changeDisc(){
+    if(currPlayer == "R")
+    {
+        change.style.backgroundColor = "yellow"
+    }
+    else{
+        change.style.backgroundColor = "red"
+
+    }
+}
+
